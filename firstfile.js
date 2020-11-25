6edb1db64781242003cd8f1f769867894c7d7550
willowphile <70330654+willowphile@users.noreply.github.com>
2020-11-25 15:04:45 -0500

    Add all files

---
+++ firstfile.js
@@ -0,0 +1,26 @@
+/*
+var user_id = 'IkoraRey';
+var user_email = 'alisyn.collins+3@gmail.com';
+var user_full_name = 'Ikora Rey';
+var user_account_id = 'Operation_Babydog'
+*/
+
+/*
+var user_id = 'AnaBray';
+var user_email = 'alisyn.collins+2@gmail.com';
+var user_full_name = 'Ana Bray';
+var user_account_id = 'Clovis_Bray_Corporation'
+*/
+
+var user_id = 'MaraSov';
+var user_email = 'alisyn.collin+1@gmail.com';
+var user_full_name = 'Queen Mara Sov';
+var user_account_id = 'Reef_Awoken'
+
+
+/*
+var user_id = 'ErisMorn';
+var user_email = 'alisyn.collins@gmail.com';
+var user_full_name = 'Ol Bleedy Eyes';
+var user_account_id = 'Hashladûn_Industries'
+*/
