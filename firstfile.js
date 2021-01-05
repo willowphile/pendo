5519ddfbe4123ebd5b1b34119adee66c1e6a1f62
willowphile <70330654+willowphile@users.noreply.github.com>
2021-01-05 12:21:15 -0500

    update
    update

--- firstfile.js
+++ firstfile.js
@@ -2,25 +2,57 @@
 var user_id = 'IkoraRey';
 var user_email = 'alisyn.collins+3@gmail.com';
 var user_full_name = 'Ikora Rey';
-var user_account_id = 'Operation_Babydog'
+var user_account_id = 'Operation_Babydog';
+var user_random_data = 'Warlock'
 */
 
-/*
+
 var user_id = 'AnaBray';
 var user_email = 'alisyn.collins+2@gmail.com';
 var user_full_name = 'Ana Bray';
-var user_account_id = 'Clovis_Bray_Corporation'
+var user_account_id = 'Clovis_Bray_Corporation';
+var user_language = 'en_GB';
+var user_random_data = 'Hunter'
+
+
+/*
+var user_id = 'ElsieBrayXO';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'Elsie Bray';
+var user_account_id = 'Clovis_Bray_Corporation';
+var user_language = 'en_GB'
 */
 
-var user_id = 'MaraSov';
-var user_email = 'alisyn.collin+1@gmail.com';
-var user_full_name = 'Queen Mara Sov';
-var user_account_id = 'Reef_Awoken'
+/*
+var user_id = 'ElsieBrayXO';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'Elsie Bray';
+var user_account_id = 'Stasis, LLC';
+var user_language = 'en_GB';
+var user_random_data = 'The Stranger'
+*/
 
+/*
+var user_id = 'MaraSov';
+var user_email = 'alisyn.collins+1@gmail.com';
+var user_full_name = 'Queen Mara Sov';
+var user_account_id = 'Reef_Awoken';
+var user_language = 'en_GB';
+var user_random_data = 'Queen'
+*/
 
 /*
 var user_id = 'ErisMorn';
 var user_email = 'alisyn.collins@gmail.com';
 var user_full_name = 'Ol Bleedy Eyes';
-var user_account_id = 'Hashladûn_Industries'
+var user_account_id = 'Hashladûn_Industries';
+var user_random_data = 'Warlock'
+*/
+
+/*
+var user_id = '1234567890-1234567890-1234567890-1234567890-InsanelyLongUserID';
+var user_email = 'alisyn.collins+1@gmail.com';
+var user_full_name = 'InsanelyLongUserID';
+var user_account_id = 'Reef_Awoken';
+var user_random_data = 'not even destiny related'
 */
