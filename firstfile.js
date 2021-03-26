a187e9d7b2befa623915278bae4528286c467408
willowphile <70330654+willowphile@users.noreply.github.com>
2021-03-26 10:24:47 -0400

    Updates

--- firstfile.js
+++ firstfile.js
@@ -1,18 +1,24 @@
+
 /*
 var user_id = 'IkoraRey';
 var user_email = 'alisyn.collins+3@gmail.com';
 var user_full_name = 'Ikora Rey';
 var user_account_id = 'Operation_Babydog';
-var user_random_data = 'Warlock'
+var user_language = 'en_GB';
+var user_random_data = 'Warlock';
+var user_boolean = true;
 */
-
 
 var user_id = 'AnaBray';
 var user_email = 'alisyn.collins+2@gmail.com';
 var user_full_name = 'Ana Bray';
 var user_account_id = 'Clovis_Bray_Corporation';
 var user_language = 'en_GB';
-var user_random_data = 'Hunter'
+var user_random_data = 'ThisIsNewData';
+var user_boolean = 0;
+
+//var user_date1 = '2020-03-27T00:00:00.000000Z';
+//var user_date2 = '2018-03-27T00:08:21.940412Z'
 
 
 /*
@@ -55,4 +61,12 @@ var user_email = 'alisyn.collins+1@gmail.com';
 var user_full_name = 'InsanelyLongUserID';
 var user_account_id = 'Reef_Awoken';
 var user_random_data = 'not even destiny related'
+*/
+
+/*
+var user_id = 'AccountlessVisitor';
+var user_email = 'alisyn.collins+4@gmail.com';
+var user_full_name = 'The Drifter';
+var user_language = 'en_GB';
+var user_random_data = 'Gambito'
 */
